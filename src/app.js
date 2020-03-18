713c0728f1b6a64ea6b390b9ee52677a191c5c0f
Kaoutar <lagdani.kaoutar@gmail.com>
2020-03-18 23:33:40 +0100

    INIT commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./styles/styles.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const templ = <p>hello hi</p>;
+ReactDOM.render(templ, document.getElementById("app"));
